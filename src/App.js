c9108f875774d1961bc2501fbc56443439befac2
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-08-04 16:37:08 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Ticker extends Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 0,
+      paused: false,
+      setInt: null
+    }
+  }
+
+  clear() {
+    this.setState({
+      count: 0
+    })
+  }
+
+  pause() {
+    if (this.state.paused) {
+      let id = setInterval(() => {
+        this.setState({
+          count: this.state.count + 1,
+          setInt: id,
+          paused: false
+        })
+      }, 1000);
+    } else {
+      clearInterval(this.state.setInt)
+      this.setState({
+        paused: true
+      })
+    }
+  }
+
+  componentDidMount() {
+    let id = setInterval(() => {
+      this.setState({
+        count: this.state.count + 1,
+        setInt: id
+      })
+    }, 1000) 
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.count % 3 === 0) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  render() {
+    return (
+      <div className='counter'>
+        <h1>The Count is: {this.state.count}</h1>
+        <div className='buttons'>
+          <button type='button' onClick={()=> this.clear()}>Clear</button>
+          <br></br>
+          <button type='button' onClick={()=> this.pause()}>
+            {this.state.paused ? 'Unpause Ticker' : 'Pause Ticker'}
+          </button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Ticker;
